80515e6a01496049986ac29cb29491b7f7a665d5
Danikh25 <dani.kha@hotmail.com>
2022-02-02 13:20:25 -0500

    Create Shell.py

---
+++ Shell.py
@@ -0,0 +1,40 @@
+import subprocess
+import os
+
+#Main function where we will define the user input
+def main():
+    while True:
+        #prefix of command
+        command = input("$ ")
+        if command == "exit":
+            break
+        elif command == "help":
+            print("psh: This is a simple shell program")
+        else:
+            execute_commands(command)
+
+    # Take the input file name from user
+    filename = input("Enter the file name\n")
+    #Check if the file exists
+    try:
+        file = open(filename, 'r')
+        #Reading all the lines
+        while True:
+            line = file.readline()
+            if line[-1:] == "\n":
+                line = line[:-1]
+            #End of line
+            if not line:
+             break
+            #close file
+            file.close()
+    #If file not found
+    except IOError:
+     print("FIle not available")
+
+def execute_commands(command):
+    try:
+        subprocess.run(command.split())
+        #Making an execption so the program doesn't crash when a unkown input is entered 
+    except Exception:
+        print("psh: command not found: {}".format(command))
